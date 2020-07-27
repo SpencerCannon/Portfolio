2c722aafd28321304a1b55f8ef3197f97819474d
Spencer Cannon <cannonspencer95@yahoo.com>
2020-07-27 12:49:52 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Components/Home';
+import Projects from './Components/Projects';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/Projects" component={Projects} />
+        <Route component={Error} />
+      </Switch>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
---
+++ src/Components/Projects.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'; 
+
+class Projects extends Component { //creating a class called Projects, Syntax.
+    state = {
+    };
+
+    render(){ 
+        return ( 
+            <>
+                <h1>Projects</h1>
+               
+            </>
+        
+        )
+    }
+}
+
+export default Projects;
+
